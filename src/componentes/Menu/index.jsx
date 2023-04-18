e9a0def70987814fef49c9415b2891a51f2d5926
Patrick Costa <pl.costa@hotmail.com>
2023-04-18 19:21:53 -0300

    Update 02 - 180423

---
+++ src/componentes/Menu/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <img src="" alt="" />
+                <a href="/"></a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
